627e9504bd499a6ed5bcdae9668dc14906a9fce9
aramsay03 <alan@aramsay.co.uk>
2020-02-24 22:52:21 +0000

    CSS Styling, MPV complete and extentions attempted

--- src/app.js
+++ src/app.js
@@ -5,21 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
     el: "#todo-app",
     data: {
       tasks: [
-          {taskname: "Write the dam code", status: false, priority: "high"},
-          {taskname: "Pull hair out", status: true, priority: "low"}
+          {taskname: "Write the dam code", isComplete: false, priority: "high"},
+          {taskname: "Pull hair out", isComplete: true, priority: "complete"},
+          {taskname: "Test data 1", isComplete: false, priority: "medium"},
+          {taskname: "PTest data 2", isComplete: false, priority: "low"}
       ],
       newTask: "",
       priority: ""
     },
     methods: {
       addNewTask: function(){
+        // if this.priority === null ? this.priority.push("low");
         this.tasks.push({
           taskname: this.newTask,
-          status: false,
+          isComplete: false,
           priority: this.priority
         });
         this.newTask = "";
         this.priority = "";
+      },
+      taskChecked: function(index){
+        this.tasks[index].priority = "complete";
       }
     }
   });
